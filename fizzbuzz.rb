743b0112eb062f23647e38d1d12c3c39301dcf80
Sarah Daniels <ssdaniels13@gmail.com>
2019-01-23 09:03:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,13 @@
 
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+if int % 5 == 0 && % 3 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   elsif int % 5 == 0
       "Buzz"
 
   end
 end
-elsif int % 5 == 0 && % 3 == 0
-    "FizzBuzz"
+
